fb713b60a78b0853babb72d587b098a643829817
Carl Timmer <timmer@jlab.org>
2011-07-12 19:09:32 +0000

    make java and C consumers and producers the same

--- java/org/jlab/coda/et/apps/Consumer.java
+++ java/org/jlab/coda/et/apps/Consumer.java
@@ -19,8 +19,6 @@ import java.nio.ByteBuffer;
 
 import org.jlab.coda.et.*;
 import org.jlab.coda.et.enums.Mode;
-import org.jlab.coda.jevio.ByteParser;
-import org.jlab.coda.jevio.EvioEvent;
 
 
 /**
@@ -35,14 +33,20 @@ public class Consumer {
 
 
     private static void usage() {
-        System.out.println("\nUsage: java Consumer -f <et name> -s <station> [-p <server port>] [-host <host>]\n\n" +
-                "       -f    ET system's name\n" +
-                "       -s    station name\n" +
-                "       -p    port number for a udp broadcast\n" +
-                "       -pos  position in station list (GC=0)\n" +
-                "       -ppos position in group of parallel staton (-1=end, -2=head)\n" +
-                "       -host host the ET system resides on (defaults to anywhere)\n" +
-                "       -nb   make station non-blocking\n\n" +
+        System.out.println("\nUsage: java Consumer -f <et name> -host <ET host> -s <station name> [-h] [-v] [-nb]\n" +
+                "                      [-p <ET server port>] [-c <chunk size>] [-q <queue size>]\n" +
+                "                      [-pos <station position>] [-ppos <parallel station position>]\n\n" +
+                "       -host  ET system's host\n" +
+                "       -f     ET system's (memory-mapped file) name\n" +
+                "       -s     create station of this name\n" +
+                "       -h     help\n" +
+                "       -v     verbose output\n" +
+                "       -nb    make station non-blocking\n" +
+                "       -p     ET server port\n" +
+                "       -c     number of events in one get/put array\n" +
+                "       -q     queue size if creating nonblocking station\n" +
+                "       -pos   position of created station in station list (1,2,...)\n" +
+                "       -ppos  position of created station within a group of parallel stations (-1=end, -2=head)\n\n" +
                 "        This consumer works by making a direct connection\n" +
                 "        to the ET system's tcp server port.\n");
     }
@@ -50,88 +54,120 @@ public class Consumer {
 
     public static void main(String[] args) {
 
-        int position = 1, pposition = 0, blocking = 1;
+        int position = 1, pposition = 0, qSize = 0, chunk = 1;
+        boolean blocking = true, verbose = false;
         String etName = null, host = null, statName = null;
         int port = EtConstants.serverPort;
+        int flowMode = EtConstants.stationSerial;
+
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equalsIgnoreCase("-f")) {
+                etName = args[++i];
+            }
+            else if (args[i].equalsIgnoreCase("-host")) {
+                host = args[++i];
+            }
+            else if (args[i].equalsIgnoreCase("-nb")) {
+                blocking = false;
+            }
+            else if (args[i].equalsIgnoreCase("-v")) {
+                verbose = true;
+            }
+            else if (args[i].equalsIgnoreCase("-s")) {
+                statName = args[++i];
+            }
+            else if (args[i].equalsIgnoreCase("-p")) {
+                try {
+                    port = Integer.parseInt(args[++i]);
+                    if ((port < 1024) || (port > 65535)) {
+                        System.out.println("Port number must be between 1024 and 65535.");
+                        usage();
+                        return;
+                    }
+                }
+                catch (NumberFormatException ex) {
+                    System.out.println("Did not specify a proper port number.");
+                    usage();
+                    return;
+                }
+            }
+            else if (args[i].equalsIgnoreCase("-c")) {
+                try {
+                    chunk = Integer.parseInt(args[++i]);
+                    if ((chunk < 1) || (chunk > 1000)) {
+                        System.out.println("Chunk size may be 1 - 1000.");
+                        usage();
+                        return;
+                    }
+                }
+                catch (NumberFormatException ex) {
+                    System.out.println("Did not specify a proper chunk size.");
+                    usage();
+                    return;
+                }
+            }
+            else if (args[i].equalsIgnoreCase("-q")) {
+                try {
+                    qSize = Integer.parseInt(args[++i]);
+                    if (qSize < 1) {
+                        System.out.println("Queue size must be > 0.");
+                        usage();
+                        return;
+                    }
+                }
+                catch (NumberFormatException ex) {
+                    System.out.println("Did not specify a proper queue size number.");
+                    usage();
+                    return;
+                }
+            }
+            else if (args[i].equalsIgnoreCase("-pos")) {
+                try {
+                    position = Integer.parseInt(args[++i]);
+                    if (position < 1) {
+                        System.out.println("Position must be > 0.");
+                        usage();
+                        return;
+                    }
+                }
+                catch (NumberFormatException ex) {
+                    System.out.println("Did not specify a proper position number.");
+                    usage();
+                    return;
+                }
+            }
+            else if (args[i].equalsIgnoreCase("-ppos")) {
+                try {
+                    pposition = Integer.parseInt(args[++i]);
+                    if (pposition < -2 || pposition == 0) {
+                        System.out.println("Parallel position must be > -3 and != 0.");
+                        usage();
+                        return;
+                    }
+                    System.out.println("FLOW moDE is ||");
+                    flowMode = EtConstants.stationParallel;
+                    if (pposition == -2) pposition = EtConstants.newHead;
+                    else if (pposition == -1) pposition = EtConstants.end;
+
+                }
+                catch (NumberFormatException ex) {
+                    System.out.println("Did not specify a proper parallel position number.");
+                    usage();
+                    return;
+                }
+            }
+            else {
+                usage();
+                return;
+            }
+        }
+
+        if (host == null || etName == null || statName == null) {
+            usage();
+            return;
+        }
 
         try {
-            for (int i = 0; i < args.length; i++) {
-                if (args[i].equalsIgnoreCase("-f")) {
-                    etName = args[++i];
-                }
-                else if (args[i].equalsIgnoreCase("-host")) {
-                    host = args[++i];
-                }
-                else if (args[i].equalsIgnoreCase("-nb")) {
-                    blocking = 0;
-                }
-                else if (args[i].equalsIgnoreCase("-s")) {
-                    statName = args[++i];
-                }
-                else if (args[i].equalsIgnoreCase("-p")) {
-                    try {
-                        port = Integer.parseInt(args[++i]);
-                        if ((port < 1024) || (port > 65535)) {
-                            System.out.println("Port number must be between 1024 and 65535.");
-                            usage();
-                            return;
-                        }
-                    }
-                    catch (NumberFormatException ex) {
-                        System.out.println("Did not specify a proper port number.");
-                        usage();
-                        return;
-                    }
-                }
-                else if (args[i].equalsIgnoreCase("-pos")) {
-                    try {
-                        position = Integer.parseInt(args[++i]);
-                    }
-                    catch (NumberFormatException ex) {
-                        System.out.println("Did not specify a proper position number.");
-                        usage();
-                        return;
-                    }
-                }
-                else if (args[i].equalsIgnoreCase("-ppos")) {
-                    try {
-                        pposition = Integer.parseInt(args[++i]);
-                    }
-                    catch (NumberFormatException ex) {
-                        System.out.println("Did not specify a proper parallel position number.");
-                        usage();
-                        return;
-                    }
-                }
-                else {
-                    usage();
-                    return;
-                }
-            }
-
-            if (host == null) {
-                host = EtConstants.hostAnywhere;
-                /*
-                try {
-                    host = InetAddress.getLocalHost().getHostName();
-                }
-                catch (UnknownHostException ex) {
-                    System.out.println("Host not specified and cannot find local host name.");
-                    usage();
-                    return;
-                }
-                */
-            }
-
-            if (etName == null) {
-                usage();
-                return;
-            }
-            else if (statName == null) {
-                usage();
-                return;
-            }
-
             // make a direct connection to ET system's tcp server
             EtSystemOpenConfig config = new EtSystemOpenConfig(etName, host, port);
 
@@ -141,10 +177,12 @@ public class Consumer {
 
             // configuration of a new station
             EtStationConfig statConfig = new EtStationConfig();
-            //statConfig.setFlowMode(Constants.stationParallel);
-            //statConfig.setCue(100);
-            if (blocking == 0) {
+            statConfig.setFlowMode(flowMode);
+            if (!blocking) {
                 statConfig.setBlockMode(EtConstants.stationNonBlocking);
+                if (qSize > 0) {
+                    statConfig.setCue(qSize);
+                }
             }
 
             // create station
@@ -156,72 +194,69 @@ public class Consumer {
             // array of events
             EtEvent[] mevs;
 
-            int num, chunk = 1, count = 0;
-            long t1, t2, totalT = 0, totalCount = 0;
+            int num, count = 0;
+            long t1=0, t2=0, time, totalT=0, totalCount=0;
             double rate, avgRate;
 
-            // initialize
+            // keep track of time
             t1 = System.currentTimeMillis();
 
-            for (int i = 0; i < 50; i++) {
-                while (count < 300000L) {
-                    // get events from ET system
-                    mevs = sys.getEvents(att, Mode.SLEEP, null, 0, chunk);
+            while (true) {
 
-                    // keep track of time
-                    if (count == 0) t1 = System.currentTimeMillis();
+                // get events from ET system
+                mevs = sys.getEvents(att, Mode.SLEEP, null, 0, chunk);
 
-                    // example of reading & printing event data
-                    if (true) {
+                // example of reading & printing event data
+                if (verbose) {
+                    for (EtEvent mev : mevs) {
+                        // Get event's data buffer
+                        // buf.limit() = length of the actual data (not buffer capacity)
+                        ByteBuffer buf = mev.getDataBuffer();
 
-                        for (EtEvent mev : mevs) {
-                            // get event's data buffer
-                            ByteBuffer buf = mev.getDataBuffer();
-                            // buf.limit() is set to the length of the actual data (not buffer capacity)
-                            ByteParser parser = new ByteParser();
-                            EvioEvent ev = parser.parseEvent(buf);
-                            System.out.println("Event = \n"+ev.toXML());
-
-//                            System.out.println("buffer cap = " + buf.capacity() + ", lim = " + buf.limit() +
-//                            ", pos = " + buf.position());
-//                            num = mev.getDataBuffer().getInt(0);
-//                            System.out.println("data byte order = " + mev.getByteOrder());
-
-//                            if (mev.needToSwap()) {
-//                                System.out.println("    data swap = " + Integer.reverseBytes(num));
-//                            }
-//                            else {
-//                                System.out.println("    data = " + num);
-//                            }
-
-//                            int[] con = mev.getControl();
-//                            for (int j : con) {
-//                                System.out.print(j + " ");
-//                            }
-//
-//                            System.out.println("pri = " + mev.getPriority());
+                        num = buf.getInt(0);
+                        System.out.println("data byte order = " + mev.getByteOrder());
+                        if (mev.needToSwap()) {
+                            System.out.println("    data needs swapping, swapped int = " + Integer.reverseBytes(num));
                         }
-                    }
+                        else {
+                            System.out.println("    data does NOT need swapping, int = " + num);
+                        }
 
-                    // put events back into ET system
-                    sys.putEvents(att, mevs);
-                    //sys.dumpEvents(att, mevs);
-                    count += mevs.length;
+                        System.out.print("control array = {");
+                        int[] con = mev.getControl();
+                        for (int j : con) {
+                            System.out.print(j + " ");
+                        }
+                        System.out.println("}");
+                    }
                 }
+
+                // put events back into ET system
+                sys.putEvents(att, mevs);
+                count += mevs.length;
 
                 // calculate the event rate
                 t2 = System.currentTimeMillis();
-                rate = 1000.0 * ((double) count) / (t2 - t1);
-                totalCount += count;
-                totalT += t2 - t1;
-                avgRate = 1000.0 * ((double) totalCount) / totalT;
-                System.out.println("rate = " + String.format("%.3g", rate) +
-                                   " Hz,   avg = " + String.format("%.3g", avgRate));
-                count = 0;
+                time = t2 - t1;
+
+                if (time > 5000) {
+                    // reset things if necessary
+                    if ( (totalCount >= (Long.MAX_VALUE - count)) ||
+                         (totalT >= (Long.MAX_VALUE - time)) )  {
+                        totalT = totalCount = count = 0;
+                        t1 = System.currentTimeMillis();
+                        continue;
+                    }
+                    rate = 1000.0 * ((double) count) / time;
+                    totalCount += count;
+                    totalT += time;
+                    avgRate = 1000.0 * ((double) totalCount) / totalT;
+                    System.out.println("rate = " + String.format("%.3g", rate) +
+                                       " Hz,  avg = " + String.format("%.3g", avgRate));
+                    count = 0;
+                    t1 = System.currentTimeMillis();
+                }
             }
-            sys.detach(att);
-            sys.removeStation(stat);
-            sys.close();
         }
         catch (Exception ex) {
             System.out.println("Error using ET system as consumer");
