a8911e5e7dfec4c92e9d72f6273177ec78e09efe
Carl Timmer <timmer@jlab.org>
2021-11-16 12:38:17 -0500

    printout

--- src/libsrc/et_mem.c
+++ src/libsrc/et_mem.c
@@ -46,7 +46,7 @@ int et_mem_create(const char *name, size_t memsize, void **pmemory, size_t *tota
   wantedsize = memsize + ET_INITIAL_SHARED_MEM_DATA_BYTES;
   num_pages  = (int) ceil( ((double) wantedsize)/pagesize );
   totalsize  = pagesize * num_pages;
-printf("et_mem_create: size = %d bytes, requested size = %d bytes\n",totalsize, memsize);
+printf("et_mem_create: size = %zu bytes, requested size = %zu bytes\n",totalsize, memsize);
 
   mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH | S_IXUSR | S_IXGRP | S_IXOTH ;
   if ((fd = open(name, O_RDWR|O_CREAT|O_EXCL, mode)) < 0) {
