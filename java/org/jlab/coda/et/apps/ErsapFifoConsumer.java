31cdb01c77230b488261bdf8059ce7188c8cbe66
Carl Timmer <timmer@jlab.org>
2022-12-15 09:12:06 -0500

    try again to fix seg fault

--- java/org/jlab/coda/et/apps/ErsapFifoConsumer.java
+++ java/org/jlab/coda/et/apps/ErsapFifoConsumer.java
@@ -12,7 +12,10 @@ package org.jlab.coda.et.apps;
 
 import org.jlab.coda.et.*;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
+import java.util.HashSet;
 
 
 /**
@@ -40,16 +43,34 @@ public class ErsapFifoConsumer {
 
     public static void main(String[] args) {
 
-        int delay=0;
+        int port=0, delay=0;
         boolean verbose=false;
-        String etName=null;
+        String etName=null, host=null;
 
         for (int i = 0; i < args.length; i++) {
             if (args[i].equalsIgnoreCase("-f")) {
                 etName = args[++i];
             }
+            else if (args[i].equalsIgnoreCase("-host")) {
+                host = args[++i];
+            }
             else if (args[i].equalsIgnoreCase("-v")) {
                 verbose = true;
+            }
+            else if (args[i].equalsIgnoreCase("-p")) {
+                try {
+                    port = Integer.parseInt(args[++i]);
+                    if ((port < 1024) || (port > 65535)) {
+                        System.out.println("Port number must be between 1024 and 65535.");
+                        usage();
+                        return;
+                    }
+                }
+                catch (NumberFormatException ex) {
+                    System.out.println("Did not specify a proper port number.");
+                    usage();
+                    return;
+                }
             }
             else if (args[i].equalsIgnoreCase("-d")) {
                 try {
@@ -79,8 +100,19 @@ public class ErsapFifoConsumer {
 
         try {
             EtSystemOpenConfig config = new EtSystemOpenConfig();
+
+            if (port == 0) {
+                port = EtConstants.serverPort;
+            }
+            config.setTcpPort(port);
             config.setNetworkContactMethod(EtConstants.direct);
-            config.setHost(EtConstants.hostLocal);
+            if (host == null) {
+                host = EtConstants.hostLocal;
+            }
+            config.setHost(host);
+            System.out.println("Direct connection to " + host);
+
+            // Defaults are to use operating system default buffer sizes and turn off TCP_NODELAY
             config.setWaitTime(0);
             config.setEtName(etName);
 
@@ -100,66 +132,106 @@ public class ErsapFifoConsumer {
             EtFifoEntry entry = new EtFifoEntry(sys, fifo);
 
             int entryCap = fifo.getEntryCapacity();
-            if (entryCap != 1) {
-                // ERROR
-            }
+            int idCount;
             //***********************/
 
             // array of events
             EtEvent[] mevs;
-            int    idCount=0, len, num, bufId;
-            long   bytes=0L, totalBytes=0L;
+
+            int    len, num, bufId;
+            long   t1=0L, t2=0L, time, totalT=0L, count=0L, totalCount=0L, bytes=0L, totalBytes=0L;
+            double rate, avgRate;
+
+            // keep track of time
+            t1 = System.currentTimeMillis();
 
             while (true) {
 
-                // get events from ET system.
+                // get events from ET system
                 fifo.getEntry(entry);
                 mevs = entry.getBuffers();
-                // only 1 event per fifo entry since we setup the ET system that way
-                EtEvent ev = mevs[0];
 
-                // Does this buffer have any data? (Set by producer)
-                // This should never happen in this case.
-                if (!ev.hasFifoData()) {
-                    // Once we hit a buffer with no data, there is no further data
-                    fifo.putEntry(entry);
-                    continue;
-                }
-                idCount++;
+                idCount = 0;
 
-                // Id associated with this buffer in this fifo entry
-                bufId = ev.getFifoId();
-
-                // Get event's data buffer
-                ByteBuffer buf = ev.getDataBuffer();
-                len = ev.getLength();
-                bytes += len;
-                totalBytes += len;
-
-                if (verbose) System.out.println("    data (len = " + ev.getLength() + ") = ");
-
-                if (buf.hasArray()) {
-                    // If using byte array you need to watch out for endianness
-                    byte[] data = ev.getData();
-                    System.out.println("data byte order = " + ev.getByteOrder());
-                    if (ev.needToSwap()) {
-                        System.out.println("    data (len = " + len + ") needs swapping");
+                // example of reading & printing event data
+                for (int i=0; i < entryCap; i++) {
+                    // Does this buffer have any data? (Set by producer)
+                    if (!mevs[i].hasFifoData()) {
+                        // Once we hit a buffer with no data, there is no further data
+                        break;
                     }
-                    else {
-                        System.out.println("    data (len = " + len + ")does NOT need swapping");
+                    idCount++;
+
+                    // Id associated with this buffer in this fifo entry
+                    bufId = mevs[i].getFifoId();
+
+                    // Get event's data buffer
+                    ByteBuffer buf = mevs[i].getDataBuffer();
+                    num = buf.getInt(0);
+                    len = mevs[i].getLength();
+                    bytes += len;
+                    totalBytes += len;
+
+                    if (verbose) {
+                        System.out.println("    data (len = " + mevs[i].getLength() + ") = " + num);
+
+                        if (buf.hasArray()) {
+                            // If using byte array you need to watch out for endianness
+                            byte[] data = mevs[i].getData();
+                            System.out.println("data byte order = " + mevs[i].getByteOrder());
+                            if (mevs[i].needToSwap()) {
+                                System.out.println("    data (len = " + len + ") needs swapping");
+                            }
+                            else {
+                                System.out.println("    data (len = " + len + ") does NOT need swapping");
+                            }
+                        }
+                        else {
+                            // Access data thru ByteBuffer and not underlying array
+                            System.out.println("    data (len = " + len + ") accessed thru ByteBuffer");
+                        }
                     }
                 }
-                else {
-                    // Access data thru buf object - not underlying array
-                    int i = buf.getInt();
+
+                // put events back into ET system
+                fifo.putEntry(entry);
+
+                count += idCount;
+
+                // calculate the event rate
+                t2 = System.currentTimeMillis();
+                time = t2 - t1;
+
+                if (time > 5000) {
+                    // reset things if necessary
+                    if ( (totalCount >= (Long.MAX_VALUE - count)) ||
+                            (totalT >= (Long.MAX_VALUE - time)) )  {
+                        bytes = totalBytes = totalT = totalCount = count = 0L;
+                        t1 = t2;
+                        continue;
+                    }
+
+                    rate = 1000.0 * ((double) count) / time;
+                    totalCount += count;
+                    totalT += time;
+                    avgRate = 1000.0 * ((double) totalCount) / totalT;
+                    // Event rates
+                    System.out.println("Events = " + String.format("%.3g", rate) +
+                            " Hz,    avg = " + String.format("%.3g", avgRate));
+
+                    // Data rates
+                    rate    = ((double) bytes) / time;
+                    avgRate = ((double) totalBytes) / totalT;
+                    System.out.println("  Data = " + String.format("%.3g", rate) +
+                            " kB/s,  avg = " + String.format("%.3g", avgRate) + "\n");
+
+                    bytes = count = 0L;
+                    t1 = System.currentTimeMillis();
                 }
 
                 if (delay > 0) {
                     Thread.sleep(delay);
                 }
-
-                // put event back into ET system
-                fifo.putEntry(entry);
             }
 //            fifo.close();
 //            sys.close();
@@ -169,5 +241,4 @@ public class ErsapFifoConsumer {
             ex.printStackTrace();
         }
     }
-    
 }
